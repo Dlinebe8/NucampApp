a7115e03e634750a6fd84d0f89465b04506a0783
Dlinebe8 <Devallineberger@gmail.com>
2020-03-05 18:15:37 -0500

    Week 4

--- components/CampsiteInfoComponent.js
+++ components/CampsiteInfoComponent.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { Text, View,ScrollView, FlatList,Modal, Button,StyleSheet,Alert,PanResponder } from 'react-native';
+import { Text, View,ScrollView, FlatList,Modal, Button,StyleSheet,Alert,PanResponder,Share } from 'react-native';
 import { Card, Icon,Rating,Input } from 'react-native-elements';
 import { connect } from 'react-redux';
 import { baseUrl } from '../shared/baseUrl';
@@ -46,6 +46,8 @@ function RenderCampsite(props) {
 
      const {campsite}=props;
      const recognizeDrag = ({dx}) => (dx < -200) ? true : false;
+     const recognizeComment=({dx}) => (dx > 200) ? true : false;
+
      const view = React.createRef();
 
      const panResponder = PanResponder.create({
@@ -75,10 +77,22 @@ function RenderCampsite(props) {
                     { cancelable: false }
                 );
             }
+            else if (recognizeComment(gestureState)) {
+                props.onShowModal();
+            }
             return true;
         }
     });
 
+    const shareCampsite = (title, message, url) => {
+        Share.share({
+            title: title,
+            message: `${title}: ${message} ${url}`,
+            url: url
+        },{
+            dialogTitle: 'Share ' + title
+        });
+    };
     if (campsite) {
         return (
             <Animatable.View animation='fadeInDown' duration={2000} delay={1000} {...panResponder.panHandlers} ref={view}>
@@ -88,6 +102,7 @@ function RenderCampsite(props) {
                 <Icon name={props.favorite ? 'heart' : 'heart-o'} type='font-awesome' color='#f50' raised reverse 
                             onPress={() => props.favorite ? console.log('Already set as a favorite') : props.markFavorite()} />
                 <Icon name='pencil' type='font-awesome' color='#5637DD' raised reverse onPress={() => props.onShowModal()} style={styles.cardItem}/>
+                <Icon name='share' type='font-awesome' color='#5637DD' raised reverse onPress={() => shareCampsite(campsite.name, campsite.description, baseUrl + campsite.image)} style={styles.cardItem}/>
                 </View>
             </Card>
             </Animatable.View>
